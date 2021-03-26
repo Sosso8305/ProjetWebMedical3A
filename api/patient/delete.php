e18deeab5b17e4ab5888835ed0bbf67816f74ae0
Mellier Clement <clement.mellier@insa-cvl.fr>
2021-03-26 17:04:24 +0100

    delete

---
+++ api/patient/delete.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../config/database.php';
+include_once '../objects/patient.php';
+
+//On crée la variable $database de type Database 
+//Connecte la base de données
+$database = new Database();
+$db = $database->getConnection();
+ 
+// On crée la variable $patient qui permettra de manipuler la base de données
+$patient = new patient($db);
+ 
+// On passe en argument à la variable $patient la valeur ‘id’ de la variable //$_POST afin de manipuler exclusivement le docteur sélectionné
+$patient->id = $_POST['id'];
+ 
+// Utilise la méthode delete sur le docteur sélectionné 
+if($patient->delete()){
+    $patient_arr=array(
+        "status" => true,
+        "message" => "Supprimé."
+    );
+}	
+// Traite le cas où la suppression s’est mal effectué
+else{
+    $patient_arr=array(
+        "status" => false,
+        "message" => "Le docteur ne peut pas être supprimé."
+    );
+}
+print_r(json_encode($patient_arr));
+?>
